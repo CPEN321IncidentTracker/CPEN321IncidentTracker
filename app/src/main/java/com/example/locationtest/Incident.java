53c1b8b649869f3b8d539ef4bb74b5a70e16ccbd
kaigelcer <kai.gelcer@gmail.com>
2020-11-09 11:24:10 -0500

    Improved efficiency in safety score computation; broke up onCreate method in MapsActivity

--- app/src/main/java/com/example/locationtest/Incident.java
+++ app/src/main/java/com/example/locationtest/Incident.java
@@ -1,6 +1,13 @@
 package com.example.locationtest;
 
+import android.location.Address;
+import android.location.Geocoder;
+
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 // Describes an incident with title, severity, and location and (latitude, longitude)
 public class Incident {
@@ -38,5 +45,13 @@ public class Incident {
         double long2 = longitude;
         double a = 0.5 - Math.cos((lat2 - lat1)*p)/2 + Math.cos(lat1*p) * Math.cos(lat2*p) * (1-Math.cos((long2-long1)*p))/2;
         return 12742 * Math.asin(Math.sqrt(a));
+    }
+
+    public boolean sameLocality(LatLng someLocation, Geocoder coder) throws IOException {
+        List<Address> incidentAddress = coder.getFromLocation(this.getLocation().latitude, this.getLocation().longitude, 1);
+        String incidentLocality = incidentAddress.get(0).getLocality();
+        List<Address> myAddress = coder.getFromLocation(someLocation.latitude, someLocation.longitude, 1);
+        String myLocality = myAddress.get(0).getLocality();
+        return(myLocality != null && myLocality.equals(incidentLocality));
     }
 }
--- app/src/main/java/com/example/locationtest/MapsActivity.java
+++ app/src/main/java/com/example/locationtest/MapsActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -35,6 +36,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -54,17 +56,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private RetrofitInterface retrofitInterface;
 
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
 
         // Initialize the search bar
         SearchView searchView;
@@ -81,38 +82,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-
-        // Initialize home button
-        Button returnHomeButton = findViewById(R.id.returnHomeButton);
-        returnHomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent homeIntent = new Intent(MapsActivity.this, MainActivity.class);
-                startActivity(homeIntent);
-            }
-        });
-
-        // Initialize Safety Score Button
-        Button safetyScoreButton = findViewById(R.id.getSafetyScoreButton);
-        safetyScoreButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                computeSafetyScore();
-            }
-        });
-
-        // Initialize Post New Incident Button
-        Button postNewIncidentButton = findViewById(R.id.addIncidentButton);
-        postNewIncidentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent postIncidentIntent = new Intent(MapsActivity.this, NewIncidentPostActivity.class);
-                postIncidentIntent.putExtra("latitude", blueMarker.getPosition().latitude);
-                postIncidentIntent.putExtra("longitude", blueMarker.getPosition().longitude);
-                startActivity(postIncidentIntent);
-
-            }
-        });
+        // Initialize buttons
+        buttonSetup();
 
         // Initialize retrofit interface for HTTP get
         Retrofit retrofit = new Retrofit.Builder()
@@ -121,113 +92,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .build();
         retrofitInterface = retrofit.create(RetrofitInterface.class);
 
-
-        // If arriving from submitting an incident, show that incident
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            Double latitude = extras.getDouble("latitude");
-            Double longitude = extras.getDouble("longitude");
-            int severity = extras.getInt("severity");
-            String title = extras.getString("title");
-            incidents.add(new Incident(title, latitude, longitude, severity));
-        }
-
-
         // Initialize Location Manager; request location update every second
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
     }
 
-    // Go to the result of search bar query
-    private boolean getQueryResult(SearchView searchView) {
-        String result = searchView.getQuery().toString();
-        List<Address> addressList = null;
-
-        if (result != null && !result.equals("")){
-            Geocoder geocoder = new Geocoder(MapsActivity.this);
-            try {
-                addressList = geocoder.getFromLocationName(result, 1);
-                Address address = addressList.get(0);
-                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                blueMarker.setPosition(latLng);
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-        return false;
-    }
-
-
-    // Calculate the safety score at the blue marker based on number of nearby incidents
-    private void computeSafetyScore() {
-        int score = 0;
-        String isSafe = "";
-        int nearbyIncidents = 0;
-
-        // Count the number of incidents
-        for (Incident i : incidents) {
-            double distance = i.distanceFrom(blueMarker.getPosition()); //Distance from blue marker
-            if (distance < nearbyDistance) {
-                nearbyIncidents+=1;
-            }
-        }
-
-        // Score from 1 to 5
-        // Many incidents -> Low score
-        // No incidents -> High score
-        if (nearbyIncidents >= 10) {
-            score = 1;
-            isSafe = "(Unsafe)";
-        } else if (nearbyIncidents >= 7) {
-            score = 2;
-        } else if (nearbyIncidents >= 4) {
-            score = 3;
-        } else if (nearbyIncidents >= 1) {
-            score = 4;
-            isSafe = "(Safe)";
-        } else {
-            score = 5;
-            isSafe = "(Very Safe)";
-        }
-
-        // Show the safety score to the user
-        String toPrint = "The safety score at this location is: " + score + "/5 " + isSafe;
-        Toast.makeText(MapsActivity.this, toPrint, Toast.LENGTH_LONG).show();
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // Display map once it is available
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
 
         // Enable all functionality for map navigation
         mMap.setMyLocationEnabled(true);
@@ -235,9 +109,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mUiSettings.setAllGesturesEnabled(true);
         mUiSettings.setZoomControlsEnabled(true);
         mUiSettings.setMyLocationButtonEnabled(true);
-        mUiSettings.setZoomGesturesEnabled(true);
-        mUiSettings.setTiltGesturesEnabled(true);
-        mUiSettings.setRotateGesturesEnabled(true);
         mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
@@ -264,7 +135,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 incidents.addAll(response.body());
 
                 // Add markers at incidents from server
-                for (Incident i: incidents) {
+                for (Incident i : incidents) {
                     mMap.addMarker(new MarkerOptions().position(i.getLocation()).title(i.getTitle() + " (" + i.getSeverity() + ")"));
                 }
                 Toast.makeText(MapsActivity.this, "Response was successful", Toast.LENGTH_LONG).show();
@@ -272,13 +143,127 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onFailure(Call<List<Incident>> call, Throwable t) {
-
                 Toast.makeText(MapsActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
-
-
     }
+
+
+    private void buttonSetup() {
+        // Initialize home button
+        Button returnHomeButton = findViewById(R.id.returnHomeButton);
+        returnHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MapsActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        // Initialize Safety Score Button
+        Button safetyScoreButton = findViewById(R.id.getSafetyScoreButton);
+        safetyScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    computeSafetyScore();
+                } catch (IOException e) {
+                    Toast.makeText(MapsActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        // Initialize Post New Incident Button
+        Button postNewIncidentButton = findViewById(R.id.addIncidentButton);
+        postNewIncidentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent postIncidentIntent = new Intent(MapsActivity.this, NewIncidentPostActivity.class);
+                postIncidentIntent.putExtra("latitude", blueMarker.getPosition().latitude);
+                postIncidentIntent.putExtra("longitude", blueMarker.getPosition().longitude);
+                startActivity(postIncidentIntent);
+
+            }
+        });
+    }
+
+
+    // Go to the result of search bar query
+    private boolean getQueryResult(SearchView searchView) {
+        String result = searchView.getQuery().toString();
+        List<Address> addressList = null;
+
+        if (result != null && !result.equals("")) {
+            Geocoder geocoder = new Geocoder(MapsActivity.this);
+            try {
+                addressList = geocoder.getFromLocationName(result, 1);
+                Address address = addressList.get(0);
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                blueMarker.setPosition(latLng);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
+
+    // Calculate the safety score at the blue marker based on number of nearby incidents
+    private void computeSafetyScore() throws IOException {
+        int score = 0;
+        String isSafe = "";
+        int nearbyIncidents = 0;
+        boolean geocoderWorking = true;
+
+
+        // Count the number of nearby incidents
+        for (Incident i : incidents) {
+            boolean sameLocality = true;
+
+            // If the geocoder is working, use it to determine if the incident is in the same
+            // locality as the user's current location
+            if (geocoderWorking) {
+                try {
+                    Geocoder coder = new Geocoder(this, Locale.getDefault());
+                    sameLocality = i.sameLocality(myLocation, coder);
+                } catch (IOException e) {
+                    geocoderWorking = false;
+                }
+            }
+
+            // If the incident is nearby, increment nearby incidents
+            if (sameLocality) {
+                if (i.distanceFrom(blueMarker.getPosition()) < nearbyDistance) {
+                    nearbyIncidents += 1;
+                }
+            }
+        }
+
+        // Score from 1 to 5
+        // Many incidents -> Low score
+        // No incidents -> High score
+        if (nearbyIncidents >= 10) {
+            score = 1;
+            isSafe = "(Unsafe)";
+        } else if (nearbyIncidents >= 7) {
+            score = 2;
+        } else if (nearbyIncidents >= 4) {
+            score = 3;
+        } else if (nearbyIncidents >= 1) {
+            score = 4;
+            isSafe = "(Safe)";
+        } else {
+            score = 5;
+            isSafe = "(Very Safe)";
+        }
+
+        // Show the safety score to the user
+        String toPrint = "The safety score at this location is: " + score + "/5 " + isSafe;
+        Toast.makeText(MapsActivity.this, toPrint, Toast.LENGTH_LONG).show();
+    }
+
 
     // Method to display current location
     private void displayCurrentLocation() {
@@ -303,12 +288,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12.0f));
     }
 
+
     @Override
     public void onLocationChanged(@NonNull Location location) {
         Log.d(TAG, "Lat: " + location.getLatitude() + " Long: " + location.getLongitude());
         myLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
     }
-
-
 }
